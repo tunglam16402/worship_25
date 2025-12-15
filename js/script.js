ec5f5818596fa4113801b2744007dc5e335f3bdd
tunglam16402 <lamnguyen16042002@gmail.com>
2025-12-15 18:10:14 +0700

    update swiper

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ async function loadComponent(id, file, callback) {
 
 loadComponent("header-component", "/components/layout/header.html", initHeader);
 
-loadComponent("hero-component", "/components/home/hero.html");
+loadComponent("hero-component", "/components/home/hero.html", initHeroSlider);
 loadComponent("introduction-component", "/components/home/introduction.html");
 loadComponent("forms-component", "/components/home/forms.html");
 loadComponent("realms-component", "/components/home/realms.html");
@@ -54,4 +54,53 @@ function initHeader() {
   toggleBtn.addEventListener("click", openMenu);
   closeBtn.addEventListener("click", closeMenu);
   mobileOverlay.addEventListener("click", closeMenu);
+}
+
+// swpier
+function initHeroSlider() {
+  const hero = document.querySelector("[data-hero]");
+  if (!hero) return;
+
+  const slides = hero.querySelectorAll(".hero-slide");
+  const pagination = hero.querySelector(".hero-pagination");
+
+  let current = 0;
+  const total = slides.length;
+  const intervalTime = 6000;
+
+  slides.forEach((_, index) => {
+    const bullet = document.createElement("span");
+    bullet.className = "bullet";
+    if (index === 0) bullet.classList.add("active");
+
+    bullet.addEventListener("click", () => {
+      goToSlide(index);
+      resetAutoSlide();
+    });
+
+    pagination.appendChild(bullet);
+  });
+
+  const bullets = pagination.querySelectorAll(".bullet");
+
+  function goToSlide(index) {
+    slides[current].classList.remove("active");
+    bullets[current].classList.remove("active");
+
+    current = index;
+
+    slides[current].classList.add("active");
+    bullets[current].classList.add("active");
+  }
+
+  function nextSlide() {
+    goToSlide((current + 1) % total);
+  }
+
+  let autoSlide = setInterval(nextSlide, intervalTime);
+
+  function resetAutoSlide() {
+    clearInterval(autoSlide);
+    autoSlide = setInterval(nextSlide, intervalTime);
+  }
 }
